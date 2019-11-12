6191c09690b4e46cd56cdb3d0b32a92b53dd6d40
JesfrinGnZ <jess2298gnz@gmail.com>
2019-11-12 15:45:16 -0600

    Avance clientes2

---
+++ NewAthziri/Inicio/responderPregunta.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styleInicio.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png"/>
+	<title>Ahtziri | Responde</title>
+</head>
+<body background="img/fondoPaper.jpg">
+
+
+
+  <div class="container">
+
+<table class="table text-center" border="0" align="center" style="align-content: center">
+
+  <tbody>
+  <tr>
+    <td><a href="index.php" style="color:#FFFFFF;">
+		 <button type="submit"><img src="img/Cuadrado.png"></button>
+	 </a></td>
+
+	 <td><a href="index.php" style="color:#FFFFFF;">
+		<button type="submit"><img src="img/Circulo.png"></button>
+	</a></td>
+    <br>
+  </tr>
+  <tr>
+		<td><a href="index.php" style="color:#FFFFFF;">
+		 <button type="submit"><img src="img/Triangulo.png"></button>
+	 </a></td>
+	 <td><a href="index.php" style="color:#FFFFFF;">
+		<button type="submit"><img src="img/Rombo.png"></button>
+	</a></td>
+    <br>
+  </tr>
+  </tbody>
+</table>
+
+
+  </div>
+
+	<script src="js/jquery.js"> </script>
+	<script src="js/bootstrap.min.js"> </script>
+
+</body>
+</html>
